a02f0be3dc3ede0d41dc72e44e6fd2a8c306dd3f
Pasidu1 <163862208+Pasidu1@users.noreply.github.com>
2024-12-21 14:02:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "DvBQBKhB#E-4XICRr8d9hlWzFGy_7qbNG2EyVhhpn_Nien3Lt6AA", // Add your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://laramd:sadeesha2006@anya.gvsp435.mongodb.net/?retryWrites=true&w=majority&appName=Anya", // Add your mongodb url
 };
